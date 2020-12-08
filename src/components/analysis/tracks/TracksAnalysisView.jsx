0864cf31cc977084a0920761dfb3e0d24b70f82e
Torzewicz, Michal <torzewicz.michal@gmail.com>
2020-12-08 22:07:59 +0100

    Track

--- src/components/analysis/tracks/TracksAnalysisView.jsx
+++ src/components/analysis/tracks/TracksAnalysisView.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import {fancyTimeFormat} from '../../../utlis/TimeUtils';
 import FilterTopListComponent from '../generic/FilterTopListComponent';
-import Chip from '@material-ui/core/Chip';
-import Tooltip from '@material-ui/core/Tooltip';
-import StarIcon from '@material-ui/icons/Stars';
+import TrackComponent from './TrackComponent';
 
 
 const TracksAnalysisView = props => {
@@ -22,81 +17,6 @@ const TracksAnalysisView = props => {
     } = props;
 
     const classes = useStyles();
-
-    const TrackComponent = ({track}) => {
-
-        const {
-            name,
-            artists,
-            uri,
-            type,
-            popularity,
-            id,
-            href,
-            explicit,
-            album,
-            preview_url,
-            duration_ms,
-            external_urls: externalUrls,
-            polishTopListRank
-        } = track;
-
-        const {images} = album;
-
-        const {
-            height,
-            width,
-            url
-        } = images[0] || {};
-
-
-        const {spotify: spotifyUrl} = externalUrls;
-
-        return (
-            <Paper className={classes.trackContainer}>
-
-                <Paper elevation={5} className={classes.imageContainer}
-                       onClick={() => window.open(spotifyUrl)}>
-                    {width > height ? (
-                        <img src={url}
-                             alt={name}
-                             style={{width: 160}}
-                        />) : (
-                        <img src={url}
-                             alt={name}
-                             style={{
-                                 height: 160
-                             }}
-                        />)}
-                </Paper>
-
-                <div className={classes.infoContainer}>
-
-                    <Typography className={classes.nameText} variant={'h5'}>{name}</Typography>
-                    <Typography className={classes.descriptionText}
-                                variant={'h6'}>{artists.map(i => i.name).join(", ")}</Typography>
-                    <Typography
-                        className={classes.descriptionText}>{`Duration: ${fancyTimeFormat(duration_ms / 1000)}`}</Typography>
-                    <Typography className={classes.descriptionText} style={{
-                        visibility: explicit ? 'visible' : 'hidden'
-                    }}>{`Explicit`}</Typography>
-                    <audio controls={'controls'}>
-                        <source src={preview_url} type={'audio/mpeg'}/>
-                    </audio>
-                </div>
-
-                <div className={classes.infoContainer}>
-                    {
-                        polishTopListRank && (
-                            <Tooltip title={`Top ${polishTopListRank} most popular in Poland`}>
-                                <Chip label={`Top ${polishTopListRank}`} icon={<StarIcon/>} variant={'outlined'}/>
-                            </Tooltip>)
-                    }
-
-                </div>
-
-            </Paper>);
-    };
 
 
     return (
